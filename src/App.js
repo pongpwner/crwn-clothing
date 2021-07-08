aea553a885d7d55d984a2466616b81546ed2f863
pongpwner <ericsshyus@gmail.com>
2021-07-08 04:17:48 -0400

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import {Switch, Route} from "react-router-dom"
 import HomePage from './pages/homepage/homepage.component'
+const HatsPage = () =>(<div>
+  <h1>HATS PAGE</h1>
+</div>)
 function App() {
   return (
     <div >
-     <HomePage></HomePage>
+      <switch>
+     <Route exact path="/" component={HomePage}/>
+     <Route path="hats" component={HatsPage}/>
+     </switch>
     </div>
   );
 }
