85db38c0336ef50a71c2d77c020f3aaa959e9887
unknown <ericsshyus@gmail.com>
2021-07-10 18:39:47 -0400

    updating app with a shopo page, collection preview component, and collection item component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import {Switch, Route} from "react-router-dom"
-import HomePage from './pages/homepage/homepage.component'
-const HatsPage = () =>(<div>
-  <h1>HATS PAGE</h1>
-</div>)
+import logo from "./logo.svg";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+
 function App() {
   return (
-    <div >
+    <div>
       <switch>
-     <Route exact path="/" component={HomePage}/>
-     <Route path="/hats" component={HatsPage}/>
-     </switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </switch>
     </div>
   );
 }
