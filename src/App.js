f01d2dfd381a4ce29dd917552e1c42d89cbb261b
pongpwner <ericsshyus@gmail.com>
2021-07-15 03:23:04 -0400

    added firestore

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component.jsx";
 import SignInAndSignUpPage from "./pages/sign-in/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import React from "react";
 
 class App extends React.Component {
@@ -18,11 +18,19 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      createUserProfileDocument(userAuth)
+      if(userAuth){
+        const userRef= await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot=>{
+          this.setState({currentUser: {id: snapShot.id,...snapShot.data()
+          }
+        })
+        });
+      }
+      this.setState({currentUser: {userAuth}})
     });
+    
   }
   componentWillUnmount() {
     this.unsubscribeFromAuth();
